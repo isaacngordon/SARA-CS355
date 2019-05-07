221fd00794c6bd566ac98cd05788f4c465ff32a2
Isaac Gordon <tzachi.gordon@gmail.com>
2019-05-06 21:56:23 -0400

    upload works

--- search-resources/fileSearch.js
+++ search-resources/fileSearch.js
@@ -1,5 +1,6 @@
 var results = [];
 var fileUploaded = false;
+var fileData = "";
 
 $(document).ready(function(err){
 
@@ -44,39 +45,32 @@ $(document).ready(function(err){
             }
             return isCompatible;
         }
-
         //read the selected file
         if (!browserSupportFileUpload()) {
             alert('The File APIs are not fully supported in this browser!');
         } else {
-            /*
-            var data = null;
-            var file = evt.target.files[0];
-            var reader = new FileReader();
-            reader.readAsText(file);
-            reader.onload = function(event) {
-                var dataAsText = event.target.result;
-                data = $.csv.toArrays(dataAsText);
-                if (data && data.length > 0) {
-                    //alert('Imported file with ' + data.length + ' rows successfully!');
-                    alert('Imported file is: ' + dataAsText);
-                    return dataAsText;
-                } else {
-                    alert('No data available to import!');
-                }
-            };
-            reader.onerror = function() {
-                alert('Unable to read ' + file.fileName);
-            };
-            */
-            console.log(evt.FileList)
+            console.log(x.files);
+            const reader = new FileReader();
+            reader.onload = function(){
+                fileData = reader.result;
+                console.log("FileData:\n","==========", fileData);
+            }
+            reader.readAsText(x.files[0]);
+            
+            fileUploaded = true;
         }
-    }, false);
+        var t = x.files[0].type;
+        uploadFileData(fileData, t, results);
+        injectResults(results);
+        console.log("File upload success.");
+    }, false); // 38
 
     
 });
 
 function injectResults(ary){
+    //check if resultsDiv has results
+    //if so clean it out
     for(var i = 0; i < ary.length; i++){
         var obj = ary[i];
         var s = document.createElement("section");
@@ -116,11 +110,131 @@ function injectResults(ary){
     }
 }
 
-function uploadFile(inputFile){
-    
-
+function uploadFileData(inputString, ftype, ary){
+    var jAry;
+    if(ftype == "application/json"){
+        jAry = parseJSONtoJSON(inputString);
+        console.log("JSONARRAY taken at JSON to JSON",jAry);
+    }else if(ftype == "text/csv"){
+        jAry = parseCSVtoJSON(inputString);
+        console.log("JSONARRAY taken at CSV to JSON ",jAry);
+    }else if(ftype == "text/xml"){
+        jAry = parseXMLtoJSON(inputString);
+        console.log("JSONARRAY taken at XML to JSON ",jAry);
+    }
+    results = jAry;
 }
 
 function downloadResults(evt){
     
 }
+
+function queryResults(queryString){
+     /*
+  if(queryString == ""){
+ 			injectResults(results);
+      return;
+ 	}
+  
+  q[] = {word1, word2,... wordN} where word is a word in the query
+  for each element in  results[]
+  	 count = 0;
+     r = results[i]
+     for each word w in q[]
+     		//use regex?
+     	  if(r.contains(word)) count += numTimesAppearence;
+     r[i].count = count;
+  
+  ary = orderByCount(results);
+  filterAryByCount(ary);
+  injectResults(ary);
+     
+      
+  */
+     if(queryString == ""){
+         injectResults(results);
+         return;
+     }
+
+     var q = queryString.split(" ");
+     for(var i = 0; i < results.length; i++){
+        var count = 0;
+        var r = results[i];
+        for(var j = 0; j < q.length; j++){
+            var t = r.title;
+            var u = r.url;
+            var d = r.description;
+            var regex = new RegExp(q[j],"gi");
+            
+            let tMatches = t.match(regex);
+            let uMatches = u.match(regex);
+            let dMatches = d.match(regex);
+        }
+     }
+}
+
+function orderByCount(jsonAry){
+    //use a simple sorting algorithm to reorder the jsonAry by the value of the field "count"
+    function byCount(x,y){
+      return x.count - y.count;
+    }
+    jsonAry.sort(byCount);
+    
+}
+      
+function parseJSONtoJSON(dataString){
+  //parse the dataString into an array of json objects
+  //input each object into objectsAry[]
+    //return objectsAry
+     var objectsAry = [
+        {"title":"Best Pizza Restaurants in New York City : Food Network ... - NYC", 
+        "url":"https://www.foodnetwork.com > Restaurants", 
+        "description":"The Best Pizza in New York City. Keste. Paulie Gee's. Totonno's Pizzeria Napolitano. Ops. Joe's Pizza. Houdini Kitchen Lab. John's of Bleecker Street. Prince Street Pizza."
+    },
+    {"title":"New York pizza - NYC - Time Out", 
+        "url":"https://www.timeout.com/newyork/restaurants/best-new-york-pizza", 
+        "description":"Nov 7, 2018 - Craving a New York pizza slice? We ranked pies from all 5 boroughs to crown the best pizza in NYC, with family-owned joints and new-age ..."
+    },
+     ];
+    
+    return objectsAry;
+    
+}
+      
+  function parseCSVtoJSON(dataString){
+    //parse the dataString into an of json objects
+  //input each object into objectsAry[]
+    //return objectsAry
+    
+    var objectsAry = [
+        {"title":"Best Pizza Restaurants in New York City : Food Network ... - NYC", 
+        "url":"https://www.foodnetwork.com > Restaurants", 
+        "description":"The Best Pizza in New York City. Keste. Paulie Gee's. Totonno's Pizzeria Napolitano. Ops. Joe's Pizza. Houdini Kitchen Lab. John's of Bleecker Street. Prince Street Pizza."
+    },
+    {"title":"New York pizza - NYC - Time Out", 
+        "url":"https://www.timeout.com/newyork/restaurants/best-new-york-pizza", 
+        "description":"Nov 7, 2018 - Craving a New York pizza slice? We ranked pies from all 5 boroughs to crown the best pizza in NYC, with family-owned joints and new-age ..."
+    },
+     ];
+    
+    return objectsAry;
+  }
+      
+  function parseXMLtoJSON(dataString){
+    //parse the dataString into an of json objects
+      //input each object into objectAry[]
+    //return objectsAry
+    var objectsAry = [
+        {"title":"Best Pizza Restaurants in New York City : Food Network ... - NYC", 
+        "url":"https://www.foodnetwork.com > Restaurants", 
+        "description":"The Best Pizza in New York City. Keste. Paulie Gee's. Totonno's Pizzeria Napolitano. Ops. Joe's Pizza. Houdini Kitchen Lab. John's of Bleecker Street. Prince Street Pizza."
+    },
+    {"title":"New York pizza - NYC - Time Out", 
+        "url":"https://www.timeout.com/newyork/restaurants/best-new-york-pizza", 
+        "description":"Nov 7, 2018 - Craving a New York pizza slice? We ranked pies from all 5 boroughs to crown the best pizza in NYC, with family-owned joints and new-age ..."
+    },
+     ];
+    
+    return objectsAry;
+    
+  }
