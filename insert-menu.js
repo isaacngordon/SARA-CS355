7473bb0435ae93bff3183d13b39c16e68a8cf44d
Isaac Gordon <tzachi.gordon@gmail.com>
2019-05-08 02:37:05 -0400

    fixed dynamic menu

--- insert-menu.js
+++ insert-menu.js
@@ -1,13 +1,26 @@
+var genesis = "";
 $(document).ready(function(err){
 
     var x = document.getElementById("menuHere");
-    var menu = generateMenu();
+    genesis = getLocalPathSuffix();
+    var menu = generateMenu(genesis);
     console.log("Menu loaded: \n");
     //console.log(menu);
     x.innerHTML = menu;
+    console.log("path is ",window.location.pathname);
+    console.log("path is ",window.location);
+    console.log("window is ",window);
 });
 
-function generateMenu(){
+function getLocalPathSuffix(){
+    let scripts = document.getElementsByTagName("script");
+    let myPath = scripts[scripts.length - 1].src;
+    let indexBeforeMe = myPath.lastIndexOf("/");
+    let g = myPath.substring(0,indexBeforeMe);
+    return g;
+}
+
+function generateMenu(suffix){
     let str = `<nav class="menu">
     <ul id="nav-ul">
         </li>
@@ -19,33 +32,33 @@ function generateMenu(){
             </div>
         </li>
         <li>
-        <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/index.html">Home</a>
+        <a href="${suffix}/index.html">Home</a>
         </li>
         <li class="dropdown">
-            <a class="dropbtn" href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/course.html">Course</a>
+            <a class="dropbtn" href="${suffix}/course.html">Course</a>
             <div class="dropdown-content">
                 <a target="_blank" href="https://learn.zybooks.com/zybook/CUNYCSCI355TeitelmanSpring2019">Zybook</a>
             </div>
         </li>
         <li class="dropdown">
-            <a class="dropbtn"  href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/search.html">Search</a>
+            <a class="dropbtn"  href="${suffix}/search.html">Search</a>
             <div class="dropdown-content">
-                <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/search-resources/dummy-search.html">Dummy Search</a>
-                <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/search-resources/file-search.html">File Search</a>
+                <a href="${suffix}/search-resources/dummy-search.html">Dummy Search</a>
+                <a href="${suffix}/search-resources/file-search.html">File Search</a>
                 <a href="#">Google API Search</a>
                 <a href="#">The Real Deal Search</a>
             </div>
         </li>  
         <li>
-            <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/browser.html">Browser</a>
+            <a href="${suffix}/browser.html">Browser</a>
         </li>
         <li class="dropdown">
-            <a class="dropbtn" href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/about.html">About</a>
+            <a class="dropbtn" href="${suffix}/about.html">About</a>
             <div class="dropdown-content">
-            <!--    <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/about-resources/isaac-gordon.html">Isaac Gordon</a>
-                <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/about-resources/ron-refael.html">Ron Refael</a> 
+            <!--    <a href="${suffix}/about-resources/isaac-gordon.html">Isaac Gordon</a>
+                <a href="${suffix}/about-resources/ron-refael.html">Ron Refael</a> 
             -->    
-                <a href="/Users/isaacgordon/Documents/Academics/CSCI/CSCI_355_Teitelman/project-files/SARA-CS355/about-resources/contact-us.html">Contact Us</a>
+                <a href="${suffix}/about-resources/contact-us.html">Contact Us</a>
             </div>
     </ul>
 </nav>`;
