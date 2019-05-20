075ddd203981296eaad43edc8b13bbc6e4098b75
Isaac Gordon <tzachi.gordon@gmail.com>
2019-05-20 01:19:35 -0400

    re arrange file structure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080
 
 //allows html, images, css, etc other static files to run within this dirname variable
 //which evaluates to the path of the files
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
 //routes
 //which url and url motifications lead you to where?
