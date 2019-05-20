f9af886b470de87fcfa42a91d17d16fbad32aa13
Isaac Gordon <tzachi.gordon@gmail.com>
2019-05-20 01:12:32 -0400

    init for Heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+//set port
+var port = process.env.PORT || 8080
+
+//allows html, images, css, etc other static files to run within this dirname variable
+//which evaluates to the path of the files
+app.use(express.static(__dirname));
+
+//routes
+//which url and url motifications lead you to where?
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+//set server listener to port
+app.listen(port, function(){
+    console.log("App listening on port " + port);
+})
